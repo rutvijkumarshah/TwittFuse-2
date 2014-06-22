2e8d64c25aaedee853c3bb9e8ba89d0173271fe2
rutvijashah <rutvijkumar.shah@gmail.com>
2014-06-22 12:01:11 -0700

    Tweet Details view

---
+++ src/com/github/rutvijkumar/twittfuse/activities/TweetDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.github.rutvijkumar.twittfuse.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.github.rutvijkumar.twittfuse.R;
+
+public class TweetDetailsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tweet_details);
+	}
+}
